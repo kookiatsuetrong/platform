89ac2df11beb3efe7542043eba0c6a76a953434e
Beer <beer@Beers-MacBook-Air.local>
2022-02-07 02:14:05 +0700

    Improve log in mechanism

--- java/code/web/LogIn.java
+++ java/code/web/LogIn.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpSession;
 import javax.persistence.Query;
 import javax.persistence.EntityManager;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -24,7 +25,8 @@ class LogIn {
 	String checkPassword(
 			String email, 
 			String password,
-			HttpSession session) {
+			HttpSession session,
+			Model model) {
 		String s =  "select m from Member m     " +
 					"where m.email = :email     " +
 					"and m.password = :password ";
@@ -33,18 +35,31 @@ class LogIn {
 		query.setParameter("email", email);
 		query.setParameter("password", Common.encrypt(password));
 		ArrayList result = (ArrayList)query.getResultList();
-		boolean success = true;
+		boolean success = false;
 		if (result.size() == 1) {
 			Member m = (Member)result.get(0);
-			// TODO: Check member type, 
-			// "unknown" must be activate before continue
-			// some kind of member will see special page
-			session.setAttribute("member", m);
+			switch (m.type) {
+				case "unknown":					
+					model.addAttribute("title",  "Unable to log in");
+					model.addAttribute("detail", "Please activate from your " +
+												"email before continue.");
+					break;
+				default:
+					success = true;
+					session.setAttribute("member", m);
+			}
 		} else {
-			success = false;
+			model.addAttribute("title",  "Unable to log in");
+			model.addAttribute("detail", "Invalid email or password. " +
+									"Please click <a href='/member-login'>" +
+									"here</a> to try again.");
 		}
 		manager.close();
-		return success ? "redirect:/member-profile" :
-						 "redirect:/member-login";
+		
+		if (success) {
+			return "redirect:/member-profile";
+		} else {
+			return "display";
+		}
 	}
 }
